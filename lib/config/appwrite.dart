018732f26fca3aa1fb33bc67fdf6a83ea0bc104d
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2025-02-20 09:36:33 +0000

    feat: Authentication

--- lib/config/appwrite.dart
+++ lib/config/appwrite.dart
@@ -1,15 +1,16 @@
 import 'package:appwrite/appwrite.dart';
 
 class AppWrite {
-  static const projectId= '67b6e82c002bae238de3';
+  static const projectId = '67b6e82c002bae238de3';
   static const endpointId = 'https://cloud.appwrite.io/v1';
-  Client client = Client();
+  static Client client = Client();
+  static late Account account;
 
-  void init() {
-    Client client = Client();
+  static init() {
+    Account account = Account(client);
     client
-      .setEndpoint(endpointId)
-      .setProject(projectId)
-      .setSelfSigned(status: true);
+        .setEndpoint(endpointId)
+        .setProject(projectId)
+        .setSelfSigned(status: true);
   }
 }
